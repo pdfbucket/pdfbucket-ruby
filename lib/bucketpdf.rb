e233dbcdffc999cb54fea816bf43488d9de3993b
sanrodari <sanrodari@gmail.com>
2016-03-30 10:14:18 -0500

    Implement sign method

--- lib/bucketpdf.rb
+++ lib/bucketpdf.rb
@@ -1,6 +1,22 @@
 require 'bucketpdf/version'
 
+require 'openssl'
+require 'digest/sha2'
+require 'base64'
+require 'uri'
+
+# Main module
 module Bucketpdf
+  HOST_BUCKET_PDF = 'potion-api-staging.herokuapp.com'
+  ORIENTATIONS = {
+    portrait: 'portrait',
+    landscape: 'landscape'
+  }
+  PAGE_SIZES = {
+    a4: 'A4',
+    letter: 'Letter'
+  }
+
   # Main class
   class Signer
     attr_reader :api_key, :api_secret
@@ -17,7 +33,35 @@ module Bucketpdf
     end
 
     def sign(url, orientation, page_size)
-      # TODO
+      signed_uri = encrypt(api_secret, url)
+
+      query = URI.encode_www_form(
+        orientation: ORIENTATIONS[orientation],
+        page_size: PAGE_SIZES[page_size],
+        api_key: api_key,
+        signed_uri: signed_uri)
+
+      URI::HTTPS.build(
+        host: HOST_BUCKET_PDF,
+        path: '/api/convert',
+        query: query).to_s
+    end
+
+    private
+
+    def encrypt(key, content)
+      binary_key = Base64.decode64(key)
+      alg = 'AES-256-CTR'
+      iv = OpenSSL::Cipher::Cipher.new(alg).random_iv
+      aes_ctr = OpenSSL::Cipher::Cipher.new(alg)
+      aes_ctr.encrypt
+      aes_ctr.key = binary_key
+      aes_ctr.iv = iv
+
+      cipher = aes_ctr.update(content)
+      cipher << aes_ctr.final
+
+      Base64.strict_encode64(iv + cipher)
     end
   end
 end
