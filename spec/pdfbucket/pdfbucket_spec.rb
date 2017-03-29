def80473df4376b6dec2160dbfb00a9bfe72557f
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-03-29 16:58:53 -0500

    add unhappy path specs

--- spec/pdfbucket/pdfbucket_spec.rb
+++ spec/pdfbucket/pdfbucket_spec.rb
@@ -28,7 +28,7 @@ RSpec.describe PDFBucket do
     end
   end
 
-  describe "PDFBucket::PDFBucket methods" do
+  describe "PDFBucket::PDFBucket valid methods" do
     let(:generate_url) { pdfbucket.generate_url(params[:uri], params[:orientation], params[:page_size], params[:margin],
                          params[:zoom], params[:pagination], params[:position], params[:alignment], params[:expires_in]) }
 
@@ -43,5 +43,36 @@ RSpec.describe PDFBucket do
     it "#generate_plain_url" do
       expect(generate_plain_url).to eq("https://api.pdfbucket.io/api/convert?orientation=landscape&page_size=A4&margin=2px&zoom=0.7&pagination=true&position=header&alignment=center&expires_in=10&api_key=PIQ7T3GOM7D36R0O67Q97UM3F0I6CPB5&uri=http%3A%2F%2Fwww.google.com&signature=9fa0abf62662ed62f7b383d7aad5bc6c892c0455")
     end
+  end
+
+  describe "PDFBucket::PDFBucket invalid object" do
+    it "has invalid alignment" do
+      generate_url = pdfbucket.generate_url(params[:uri], params[:orientation], params[:page_size], params[:margin], params[:zoom], params[:pagination], params[:position], "", params[:expires_in])
+      body = http_body(generate_url)
+      expect(body).to include("Invalid alignment, posible values are: left, center or right")
+   end
+
+    it "has invalid position" do
+      generate_url = pdfbucket.generate_url(params[:uri], params[:orientation], params[:page_size], params[:margin], params[:zoom], params[:pagination], "", params[:alignment], params[:expires_in])
+      body = http_body(generate_url)
+      expect(body).to include("Invalid position, posible values are: header or footer")
+    end
+
+    it "has invalid pagination" do
+      generate_url = pdfbucket.generate_url(params[:uri], params[:orientation], params[:page_size], params[:margin], params[:zoom], "", params[:position], params[:alignment], params[:expires_in])
+      body = http_body(generate_url)
+      expect(body).to include("Invalid pagination, posible values are: true or false")
+    end
+
+    it "has invalid orientation" do
+      generate_url = pdfbucket.generate_url(params[:uri], "", params[:page_size], params[:margin], params[:zoom], params[:pagination], params[:position], params[:alignment], params[:expires_in])
+      body = http_body(generate_url)
+      expect(body).to include("Invalid orientation, posible values are: landscape or portrait")
+    end
+  end
+
+  def http_body(url)
+    uri = URI(url)
+    Net::HTTP.get_response(uri).body
   end
 end
