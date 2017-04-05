74344038046b2d255f56384ac6c2f4eeaae21cfa
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-04-05 16:40:29 -0500

    refactor and allow request without optional params

--- lib/pdfbucket.rb
+++ lib/pdfbucket.rb
@@ -7,7 +7,8 @@ require 'uri'
 
 # Main module
 module PDFBucket
-  DEFAULT_HOST = 'api.pdfbucket.io'
+  #DEFAULT_HOST = 'api.pdfbucket.io'
+  DEFAULT_HOST = '8ed3489f.ngrok.io'
   ORIENTATIONS = {
     portrait: 'portrait',
     landscape: 'landscape'
@@ -31,67 +32,53 @@ module PDFBucket
     attr_reader :api_key, :api_secret, :api_host
 
     def initialize(
-      api_key: ENV['PDF_BUCKET_API_KEY'],
+      api_key:    ENV['PDF_BUCKET_API_KEY'],
       api_secret: ENV['PDF_BUCKET_API_SECRET'],
-      api_host: ENV['PDF_BUCKET_API_HOST'])
+      api_host:   ENV['PDF_BUCKET_API_HOST'])
 
       fail 'bucket api_key is required' if api_key.nil? || api_key.strip.empty?
       fail 'bucket api_secret is required' if api_secret.nil? || api_secret.strip.empty?
 
-      @api_host = api_host || DEFAULT_HOST
-      @api_key = api_key
+      @api_host   = api_host || DEFAULT_HOST
+      @api_key    = api_key
       @api_secret = api_secret
     end
 
-    def generate_url(url, orientation, page_size, margin, zoom, pagination, position, alignment, expires_in, cache = nil)
+    def generate_url(url, orientation, page_size, margin, zoom, expires_in = 0, pagination = false, position = nil, alignment = nil, cache = nil)
       encrypted_uri = encrypt(api_secret, url)
 
       params = {
-        orientation: ORIENTATIONS[orientation],
-        page_size: PAGE_SIZES[page_size],
-        margin: margin,
-        zoom: zoom,
-        pagination: pagination.to_s,
-        position: POSITIONS[position],
-        alignment: ALIGNMENTS[alignment],
-        expires_in: expires_in,
-        api_key: api_key,
-        encrypted_uri: encrypted_uri
+        orientation:    ORIENTATIONS[orientation],
+        page_size:      PAGE_SIZES[page_size],
+        margin:         margin,
+        zoom:           zoom,
+        expires_in:     expires_in,
+        api_key:        api_key,
+        encrypted_uri:  encrypted_uri
       }
-      params.merge!(cache: cache) if cache
-
-      URI::HTTPS.build(
-        host: api_host,
-        path: '/api/convert',
-        query: URI.encode_www_form(params)).to_s
+      set_optional_params(params, pagination, position, alignment, cache)
+      build_uri(params)
     end
 
-    def generate_plain_url(url, orientation, page_size, margin, zoom, pagination, position, alignment, expires_in, cache = nil)
-      signature = sign(api_secret, api_key, url, orientation, page_size, margin, zoom, pagination, position, alignment, expires_in)
+    def generate_plain_url(url, orientation, page_size, margin, zoom, expires_in = 0, pagination = false, position = nil, alignment = nil, cache = nil)
+      signature = sign(api_secret, api_key, url, orientation, page_size, margin, zoom, pagination, position, alignment)
 
       params = {
-        orientation: ORIENTATIONS[orientation],
-        page_size: PAGE_SIZES[page_size],
-        margin: margin,
-        zoom: zoom,
-        pagination: pagination.to_s,
-        position: POSITIONS[position],
-        alignment: ALIGNMENTS[alignment],
-        expires_in: expires_in,
-        api_key: api_key,
-        uri: url,
-        signature: signature
+        orientation:  ORIENTATIONS[orientation],
+        page_size:    PAGE_SIZES[page_size],
+        margin:       margin,
+        zoom:         zoom,
+        expires_in:   expires_in,
+        api_key:      api_key,
+        uri:          url,
+        signature:    signature
       }
-      params.merge!(cache: cache) if cache
-
-      URI::HTTPS.build(
-        host: api_host,
-        path: '/api/convert',
-        query: URI.encode_www_form(params)).to_s
+      set_optional_params(params, pagination, position, alignment, cache)
+      build_uri(params)
     end
 
     private
-    def sign(api_secret, api_key, url, orientation, page_size, margin, zoom, pagination, position, alignment, expires_in)
+    def sign(api_secret, api_key, url, orientation, page_size, margin, zoom, pagination, position, alignment)
       params = [
         api_key,
         url,
@@ -120,6 +107,24 @@ module PDFBucket
       cipher << aes_ctr.final
 
       Base64.strict_encode64(iv + cipher)
+    end
+
+    def build_uri(params)
+      URI::HTTPS.build(
+        host: api_host,
+        path: '/api/convert',
+        query: URI.encode_www_form(params)).to_s
+    end
+
+    def set_optional_params(params, pagination, position, alignment, cache)
+      params.merge!(cache: cache) if cache
+      if pagination
+        params.merge!({
+          pagination: pagination,
+          position:   POSITIONS[position],
+          alignment:  ALIGNMENTS[alignment]
+        })
+      end
     end
   end
 end
