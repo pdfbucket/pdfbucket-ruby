955d40cf2afbe8ba62418dcf1bc44256d9fb5bd4
Andres Vizcaino <andruvizcaino1989@gmail.com>
2017-04-05 16:40:40 -0500

    Update specs according optional params

--- spec/pdfbucket/pdfbucket_spec.rb
+++ spec/pdfbucket/pdfbucket_spec.rb
@@ -1,8 +1,9 @@
 require "spec_helper"
 
 RSpec.describe PDFBucket do
-  API_KEY     = "PIQ7T3GOM7D36R0O67Q97UM3F0I6CPB5"
-  API_SECRET  = "HieMN8dvi5zfSbKvqxKccxDo3LozqOIrY59U/jrZY54="
+  DEFAULT_HOST  = "api.pdfbucket.io"
+  API_KEY       = "22KAQQVVBR8JB731AINT5T1BPV6HCSFG"
+  API_SECRET    = "tLW0yQlyRvQ4OEO7azBtE2llYPo/KXh3+W7jjXiAsdk="
   let(:params) do
     {
       uri:          "http://www.google.com",
@@ -10,62 +11,60 @@ RSpec.describe PDFBucket do
       page_size:    :a4,
       margin:       "2px",
       zoom:         "0.7",
-      pagination:   "true",
+      expires_in:   10,
+      pagination:   true,
       position:     :header,
       alignment:    :center,
-      expires_in:   10,
       cache:        0
     }
   end
 
-  let(:pdfbucket) { PDFBucket::PDFBucket.new(api_key: API_KEY, api_secret: API_SECRET) }
+  let(:pdfbucket) { PDFBucket::PDFBucket.new(api_key: API_KEY, api_secret: API_SECRET, api_host: DEFAULT_HOST) }
 
   describe "Initial object" do
     it "instantiate a PDFBucket::PDFBucket with and api_host, api_key and api_secret" do
-      expect(pdfbucket.api_host).to eq("api.pdfbucket.io")
+      expect(pdfbucket.api_host).to eq(DEFAULT_HOST)
       expect(pdfbucket.api_key).to eq(API_KEY)
       expect(pdfbucket.api_secret).to eq(API_SECRET)
     end
   end
 
   describe "PDFBucket::PDFBucket valid methods" do
-    let(:generate_url) { pdfbucket.generate_url(params[:uri], params[:orientation], params[:page_size], params[:margin],
-                         params[:zoom], params[:pagination], params[:position], params[:alignment], params[:expires_in]) }
-
-    it "#generate_url" do
-      encrypted_uri = generate_url.split("encrypted_uri").last
-      expect(generate_url).to eq("https://api.pdfbucket.io/api/convert?orientation=landscape&page_size=A4&margin=2px&zoom=0.7&pagination=true&position=header&alignment=center&expires_in=10&api_key=PIQ7T3GOM7D36R0O67Q97UM3F0I6CPB5&encrypted_uri#{encrypted_uri}")
+    it "#generate_url without optional params" do
+      optional_params_uri = pdfbucket.generate_url(params[:uri], params[:orientation], params[:page_size], params[:margin], params[:zoom])
+      encrypted_uri = optional_params_uri.split("encrypted_uri=").last
+      expect(optional_params_uri).to eq("https://#{DEFAULT_HOST}/api/convert?orientation=landscape&page_size=A4&margin=2px&zoom=0.7&expires_in=0&api_key=#{API_KEY}&encrypted_uri=#{encrypted_uri}")
     end
 
-    let(:generate_plain_url) { pdfbucket.generate_plain_url(params[:uri], params[:orientation], params[:page_size], params[:margin],
-                              params[:zoom], params[:pagination], params[:position], params[:alignment], params[:expires_in]) }
+    let(:generate_url) { pdfbucket.generate_url(params[:uri], params[:orientation], params[:page_size], params[:margin], params[:zoom], params[:expires_in], params[:pagination], params[:position], params[:alignment]) }
+
+    it "#generate_url" do
+      encrypted_uri = generate_url.split("encrypted_uri=").last.split("&pagination").first
+      expect(generate_url).to eq("https://#{DEFAULT_HOST}/api/convert?orientation=landscape&page_size=A4&margin=2px&zoom=0.7&expires_in=10&api_key=#{API_KEY}&encrypted_uri=#{encrypted_uri}&pagination=true&position=header&alignment=center")
+    end
+
+    let(:generate_plain_url) { pdfbucket.generate_plain_url(params[:uri], params[:orientation], params[:page_size], params[:margin], params[:zoom], params[:expires_in], params[:pagination], params[:position], params[:alignment]) }
 
     it "#generate_plain_url" do
-      expect(generate_plain_url).to eq("https://api.pdfbucket.io/api/convert?orientation=landscape&page_size=A4&margin=2px&zoom=0.7&pagination=true&position=header&alignment=center&expires_in=10&api_key=PIQ7T3GOM7D36R0O67Q97UM3F0I6CPB5&uri=http%3A%2F%2Fwww.google.com&signature=9fa0abf62662ed62f7b383d7aad5bc6c892c0455")
+      expect(generate_plain_url).to eq("https://#{DEFAULT_HOST}/api/convert?orientation=landscape&page_size=A4&margin=2px&zoom=0.7&expires_in=10&api_key=#{API_KEY}&uri=http%3A%2F%2Fwww.google.com&signature=8cf3be71d48bd12ba3103ccc69b77424ec8adaf8&pagination=true&position=header&alignment=center")
     end
   end
 
   describe "PDFBucket::PDFBucket invalid object" do
     it "has invalid alignment" do
-      generate_url = pdfbucket.generate_url(params[:uri], params[:orientation], params[:page_size], params[:margin], params[:zoom], params[:pagination], params[:position], "", params[:expires_in])
+      generate_url = pdfbucket.generate_url(params[:uri], params[:orientation], params[:page_size], params[:margin], params[:zoom], params[:expires_in], params[:pagination], params[:position], "")
       body = http_body(generate_url)
       expect(body).to include("Invalid alignment, posible values are: left, center or right")
    end
 
     it "has invalid position" do
-      generate_url = pdfbucket.generate_url(params[:uri], params[:orientation], params[:page_size], params[:margin], params[:zoom], params[:pagination], "", params[:alignment], params[:expires_in])
+      generate_url = pdfbucket.generate_url(params[:uri], params[:orientation], params[:page_size], params[:margin], params[:zoom], params[:expires_in], params[:pagination], "", params[:alignment])
       body = http_body(generate_url)
       expect(body).to include("Invalid position, posible values are: header or footer")
     end
 
-    it "has invalid pagination" do
-      generate_url = pdfbucket.generate_url(params[:uri], params[:orientation], params[:page_size], params[:margin], params[:zoom], "", params[:position], params[:alignment], params[:expires_in])
-      body = http_body(generate_url)
-      expect(body).to include("Invalid pagination, posible values are: true or false")
-    end
-
     it "has invalid orientation" do
-      generate_url = pdfbucket.generate_url(params[:uri], "", params[:page_size], params[:margin], params[:zoom], params[:pagination], params[:position], params[:alignment], params[:expires_in])
+      generate_url = pdfbucket.generate_url(params[:uri], "", params[:page_size], params[:margin], params[:zoom], params[:expires_in], params[:pagination], params[:position], params[:alignment])
       body = http_body(generate_url)
       expect(body).to include("Invalid orientation, posible values are: landscape or portrait")
     end
